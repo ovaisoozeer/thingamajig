2d1ced573bf8f153e3b36f6a3fb644e0c78e3aa0
Ovais Oozeer <ovaisoozeer@users.noreply.github.com>
2024-05-16 15:49:10 +0100

    Adds Customer Identity stack with user pool

--- thingamajig/bin/thingamajig.ts
+++ thingamajig/bin/thingamajig.ts
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
-import { ThingamajigStack } from "../lib/thingamajig-stack";
+import { CustomerIdentityStack } from "../lib/customer-identity-stack";
 
 const app = new cdk.App();
 const CDK_DEFAULT_ACCOUNT = "654654391715";
 const CDK_DEFAULT_REGION = "eu-west-2";
 
 // Definition of top-level application "stack".
-new ThingamajigStack(app, "ThingamajigStack", {
+new CustomerIdentityStack(app, "CustomerIdentityStack", {
   env: { account: CDK_DEFAULT_ACCOUNT, region: CDK_DEFAULT_REGION },
 });
