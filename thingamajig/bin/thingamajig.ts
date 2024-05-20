16e4684a61dc8fafc84998a0757ab9c3ea0cb0e2
Ovais Oozeer <ovaisoozeer@users.noreply.github.com>
2024-05-20 14:11:12 +0100

    Adds tags to customer user pool

--- thingamajig/bin/thingamajig.ts
+++ thingamajig/bin/thingamajig.ts
@@ -7,7 +7,18 @@ const app = new cdk.App();
 const CDK_DEFAULT_ACCOUNT = "654654391715";
 const CDK_DEFAULT_REGION = "eu-west-2";
 
-// Definition of top-level application "stack".
+const applicationName = "thingamajig";
+const theYear3000 = "3000-01-01 00:00:00";
+const dev = "env-dev";
+
 new CustomerIdentityStack(app, "CustomerIdentityStack", {
   env: { account: CDK_DEFAULT_ACCOUNT, region: CDK_DEFAULT_REGION },
+  tags: {
+    application: applicationName,
+    "creation date": "2024-05-16 16:00:00",
+    criticality: "criticality-required",
+    "data scope": "data-privileged",
+    environment: dev,
+    "expiry date": theYear3000,
+  },
 });
