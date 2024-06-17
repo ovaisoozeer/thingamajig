cd642401c6b115545ec9fb16b7648915f90f9da1
Ovais Oozeer <ovaisoozeer@users.noreply.github.com>
2024-06-17 16:55:44 +0100

    Non-deployable version of codebase - giving up

--- thingamajig/lib/customer-account-service.ts
+++ thingamajig/lib/customer-account-service.ts
@@ -1,22 +1,26 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import {
-  IVpc,
   SecurityGroup,
   Vpc,
   InstanceType,
   InstanceSize,
   InstanceClass,
+  Subnet,
 } from "aws-cdk-lib/aws-ec2";
 import {
   Credentials,
   DatabaseInstance,
   DatabaseInstanceEngine,
+  DatabaseProxy,
+  ProxyTarget,
   SubnetGroup,
 } from "aws-cdk-lib/aws-rds";
 import { AccountValues } from "../magic-strings";
-import { RdsDataSource } from "aws-cdk-lib/aws-appsync";
-import { Duration } from "aws-cdk-lib";
+import { Duration, RemovalPolicy } from "aws-cdk-lib";
+import { LambdaRestApi } from "aws-cdk-lib/aws-apigateway";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import { Secret } from "aws-cdk-lib/aws-secretsmanager";
 
 export class CustomerAccountService extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -26,15 +30,38 @@ export class CustomerAccountService extends cdk.Stack {
       vpcId: AccountValues.DEV_VPC_ID,
     });
 
-    const dbSubnetGroup = SubnetGroup.fromSubnetGroupName(
+    const vpcSubnet1 = Subnet.fromSubnetId(
       this,
-      "DatabaseSubnetGroup",
-      AccountValues.RDS_DATABASE_SUBNET_GROUP_NAME
+      "privateSubnet1",
+      AccountValues.DEV_VPC_PRIVATE_SUBNET_1_ID
+    );
+
+    const vpcSubnet2 = Subnet.fromSubnetId(
+      this,
+      "privateSubnet2",
+      AccountValues.DEV_VPC_PRIVATE_SUBNET_2_ID
+    );
+
+    const subnetGroupName = "ThingamajigCustomerAccountsDBSubnetGroup";
+    const dbSubnetGroup = new SubnetGroup(
+      this,
+      "CustomerAccountsDBSubnetGroup",
+      {
+        subnetGroupName: subnetGroupName,
+        description: "Subnet group for thingamajig customer accounts db",
+        vpc: devVpc,
+        vpcSubnets: {
+          // onePerAz: true,
+          // subnetGroupName: subnetGroupName,
+          subnets: [vpcSubnet1, vpcSubnet2],
+          // subnetType: SubnetType.PRIVATE_ISOLATED,
+        },
+      }
     );
 
     const dbSecurityGroup = SecurityGroup.fromLookupByName(
       this,
-      "DatabaseSecurityGroup",
+      "DevDatabaseSecurityGroup",
       AccountValues.RDS_DATABASE_SECURITY_GROUP_NAME,
       devVpc
     );
@@ -51,6 +78,45 @@ export class CustomerAccountService extends cdk.Stack {
       backupRetention: Duration.days(30),
       allowMajorVersionUpgrade: true,
       allocatedStorage: 20,
+      removalPolicy: RemovalPolicy.DESTROY,
     });
+
+    const dbProxySecret: Secret = new Secret(
+      this,
+      "CustomerAccountsDBProxySecret",
+      {
+        description: "Secret for rds proxy to thingamajig customer accounts db",
+        removalPolicy: RemovalPolicy.DESTROY,
+        secretName: "customer-accounts-db-proxy-secret",
+      }
+    );
+
+    const dbProxy = new DatabaseProxy(this, "CustomerAccountsDBProxy", {
+      proxyTarget: ProxyTarget.fromInstance(database),
+      secrets: [dbProxySecret],
+      securityGroups: [dbSecurityGroup],
+      vpc: devVpc,
+    });
+
+    const restApiHandler = new lambda.Function(
+      this,
+      "CustomerAccountsApiHandler",
+      {
+        runtime: lambda.Runtime.NODEJS_20_X,
+        code: lambda.Code.fromAsset("lambda"),
+        handler: "customer-account-service.restApi",
+      }
+    );
+
+    const restApi: LambdaRestApi = new LambdaRestApi(
+      this,
+      "CustomerAccountsApi",
+      {
+        handler: restApiHandler,
+        restApiName: "CustomerAccountsApi",
+      }
+    );
+
+    // dbProxy.grantConnect(restApiHandler);
   }
 }
--- thingamajig/magic-strings.ts
+++ thingamajig/magic-strings.ts
@@ -1,7 +1,8 @@
 export const AccountValues: { [id: string]: string } = {
   DEV_VPC_ID: "vpc-04d96d6a8ca692320",
-  RDS_DATABASE_SUBNET_GROUP_NAME: "dev-databases",
   RDS_DATABASE_SECURITY_GROUP_NAME: "dev-database-access",
+  DEV_VPC_PRIVATE_SUBNET_1_ID: "subnet-0aed42035c196a175",
+  DEV_VPC_PRIVATE_SUBNET_2_ID: "subnet-0810777397562a033",
   PREFERRED_REGION: "eu-west-2",
   ACCOUNT_NUMBER: "654654391715",
   APPLICATION_NAME: "Thingamajig",
